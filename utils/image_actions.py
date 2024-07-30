ad50d31696a07ed318f5beead2f5afd0bb3cb80c
derefff <deref122@gmail.com>
2024-07-30 19:49:17 +0200

    settings with determinating images sizes, implementation in cli combine function

--- utils/image_actions.py
+++ utils/image_actions.py
@@ -1,5 +1,42 @@
 from PIL import Image, ImageFont, ImageDraw 
 
+settings = {
+    'images': [],
+    'image-folder': ''
+    'rows': 2,
+    'cols': 2,
+    'gapX': 4,
+    'gapY': 4,
+    'output-name': 'output.png'
+}
 
 def load_img(name):
     return Image.open(name)
+
+#getting the min width and min height 
+def determine_size(images):
+    width = images[0].width
+    height = images[0].height
+    for img in images:
+        if img.width < width:
+            width = img.width
+        
+        if img.height < height:
+            height = img.height
+
+    return width, height
+
+'''
+1. load images
+2. dtermine/adjust image sizes
+3. marge and save images
+'''
+
+def cli_image_combine():
+    # here will go everyting
+    images = []
+
+    for i in settings['images']:
+        images.append(load_img(i))
+
+    width, height = determine_size(images)
